292b407fcea660498d3bbdd9f84f3394c1673cde
Nick Young <nick.young@auckland.ac.nz>
2020-03-25 10:28:11 +1300

    Updates

---
+++ precache-manifest.274a7c7978f5343ab7bf1e671fcd5e2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2723f564ec40e3db35d5a5534b36876",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a32292a354add713a618",
+    "url": "./static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "35d45bde9954a76bd41f",
+    "url": "./static/css/main.2c72b021.chunk.css"
+  },
+  {
+    "revision": "a32292a354add713a618",
+    "url": "./static/js/2.c8fb4543.chunk.js"
+  },
+  {
+    "revision": "a13d492d052991f4082249688f816bfa",
+    "url": "./static/js/2.c8fb4543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec597d8a98085b1f520",
+    "url": "./static/js/3.b4a8ca16.chunk.js"
+  },
+  {
+    "revision": "35d45bde9954a76bd41f",
+    "url": "./static/js/main.9abf787a.chunk.js"
+  },
+  {
+    "revision": "ab4c123cab870302e000",
+    "url": "./static/js/runtime-main.e347226c.js"
+  },
+  {
+    "revision": "d652360aef08641a922ceb9a61b8e82f",
+    "url": "./static/media/notes.d652360a.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.756ccc5b9cd394537794df8a2165e9f1.js"
+  "./precache-manifest.274a7c7978f5343ab7bf1e671fcd5e2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
